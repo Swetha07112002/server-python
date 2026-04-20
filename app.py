301d3dfc5cfeb19e70c1d69307a3f041fdf62c0c
Swetha07112002 <swethaamirthapandi2002@gmail.com>
2026-04-20 15:14:13 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, request, jsonify
+from datetime import datetime
+
+app = Flask(__name__)
+
+# Dummy license store
+licenses = {
+    "TEST123": {
+        "hwid": "36a194963c3ba3e68f2d647782c1cb6fff533fd370ade1179dc59d1bc74fe9fb"
+    }
+}
+
+logs = []
+
+@app.route("/")
+def home():
+    return "License Server Running"
+
+@app.route("/verify", methods=["POST"])
+def verify():
+    data = request.json
+
+    key = data.get("licenseKey")
+    hwid = data.get("hwid")
+
+    if key in licenses and licenses[key]["hwid"] == hwid:
+        return jsonify({"valid": True})
+
+    return jsonify({"valid": False})
+
+@app.route("/log", methods=["POST"])
+def log():
+    data = request.json
+
+    logs.append({
+        "hwid": data.get("hwid"),
+        "status": data.get("status"),
+        "date": datetime.now().strftime("%Y-%m-%d"),
+        "time": datetime.now().strftime("%I:%M %p")
+    })
+
+    return "OK"
+
+@app.route("/logs", methods=["GET"])
+def get_logs():
+    return jsonify(logs)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
